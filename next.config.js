6212b07e5d05e39fc803094464aedf084ca914f6
nuhptr <nugrahaadi733@gmail.com>
2023-10-31 23:21:37 +0700

    config/domain&experimental

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+   experimental: {
+      serverComponentsExternalPackages: ["mongosee"],
+   },
+   images: {
+      remotePatterns: [{ protocol: "https", hostname: "m.media-amazon.com" }],
+   },
+}
 
 module.exports = nextConfig
