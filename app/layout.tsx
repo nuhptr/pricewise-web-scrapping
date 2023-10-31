6cf09400286cc1dcdfae818842c3135c6244f696
nuhptr <nugrahaadi733@gmail.com>
2023-10-31 20:18:34 +0700

    app/layout update code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import { Inter, Space_Grotesk } from "next/font/google"
 
 import "./globals.css"
 
+import Navbar from "@/components/Navbar"
+
 const inter = Inter({ subsets: ["latin"] })
+const spaceGrotesk = Space_Grotesk({
+   subsets: ["latin"],
+   weight: ["300", "400", "500", "600", "700"],
+})
 
 export const metadata: Metadata = {
    title: "Pricewise | Track prices of your favorite products",
@@ -14,7 +20,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
    return (
       <html lang="en">
          <body className={inter.className} suppressHydrationWarning>
-            {children}
+            <main className="max-w-10xl mx-auto">
+               <Navbar />
+               {children}
+            </main>
          </body>
       </html>
    )
