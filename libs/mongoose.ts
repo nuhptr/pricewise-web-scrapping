50d67446f0719c607d6fb428a505d5bf6e22e28e
nuhptr <nugrahaadi733@gmail.com>
2023-11-01 06:07:15 +0700

    libs/add some file

---
+++ libs/mongoose.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+
+let isConnected = false // Variable to track connection status
+
+export const connectToDB = async () => {
+   mongoose.set("strictQuery", true)
+
+   if (!process.env.MONGODB_URI) return console.log("No MongoDB URI provided")
+   if (isConnected) return console.log("=> Using existing database connection")
+
+   try {
+      await mongoose.connect(process.env.MONGODB_URI)
+      isConnected = true
+      console.log("=> MongoDB connection established")
+   } catch (error: any) {
+      console.log("=> MongoDB connection error:", error.message)
+   }
+}
--- libs/scraper/index.ts
+++ libs/scraper/index.ts
@@ -1,7 +1,9 @@
+"use server"
+
 import axios from "axios"
 import * as cheerio from "cheerio"
 
-import { extractCurrency, extractPrice } from "../utils"
+import { extractCurrency, extractDescription, extractPrice } from "../utils"
 
 export async function scrapeAmazonProduct(url: string) {
    if (!url) return
@@ -31,9 +33,8 @@ export async function scrapeAmazonProduct(url: string) {
       const title = $("#productTitle").text().trim()
       const currentPrice = extractPrice(
          $(".priceToPay span.a-price-whole"),
-         $("a.size.base.a-color-price"),
-         $(".a-button-selected .a-color-base"),
-         $(".a-price.a-text-price")
+         $(".a.size.base.a-color-price"),
+         $(".a-button-selected .a-color-base")
       )
 
       const originalPrice = extractPrice(
@@ -41,34 +42,41 @@ export async function scrapeAmazonProduct(url: string) {
          $(".a-price.a-text-price span.a-offscreen"),
          $("#listPrice"),
          $("#priceblock_dealprice"),
-         $(".a-size-base.a.color-price")
+         $(".a-size-base.a-color-price")
       )
 
-      const outOfStock = $("#availability span").text().trim() === "Currently unavailable."
+      const outOfStock = $("#availability span").text().trim().toLowerCase() === "currently unavailable"
 
-      const image =
+      const images =
          $("#imgBlkFront").attr("data-a-dynamic-image") || $("#landingImage").attr("data-a-dynamic-image") || "{}"
-      const imageUrls = Object.keys(JSON.parse(image))
+      const imageUrls = Object.keys(JSON.parse(images))
 
       const currency = extractCurrency($(".a-price-symbol"))
       const discountRate = $(".savingsPercentage").text().replace(/[-%]/g, "")
+
+      const description = extractDescription($)
 
       const data = {
          url,
          currency: currency || "$",
          image: imageUrls[0],
-         title: title,
-         currentPrice: Number(currentPrice),
-         originalPrice: Number(originalPrice),
+         title,
+         currentPrice: Number(currentPrice) || Number(originalPrice),
+         originalPrice: Number(originalPrice) || Number(currentPrice),
          priceHistory: [],
          discountRate: Number(discountRate),
          category: "category",
          reviewsCount: 100,
          stars: 4.5,
          isOutOfStock: outOfStock,
+         description,
+         lowestPrice: Number(currentPrice) || Number(originalPrice),
+         highestPrice: Number(originalPrice) || Number(currentPrice),
+         averagePrice: Number(currentPrice) || Number(originalPrice),
       }
 
       console.log(data)
+      return data
    } catch (error: any) {
       throw new Error(`Failed to scrape product: ${error.message}`)
    }
