4e8616c32dbadbbf88bd79cf40737df40d8099bd
nuhptr <nugrahaadi733@gmail.com>
2023-11-01 12:35:40 +0700

    page/Home update code

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,13 @@ import Image from "next/image"
 
 import HeroCarousel from "@/components/HeroCarousel"
 import Searchbar from "@/components/Searchbar"
+import ProductCard from "@/components/ProductCard"
 
-const Home = () => {
+import { getAllProducts } from "@/libs/actions"
+
+const Home = async () => {
+   const allProducts = await getAllProducts()
+
    return (
       <>
          <section className="px-6 py-24 md:px-20">
@@ -13,11 +18,13 @@ const Home = () => {
                      Smart Shopping Starts Here:
                      <Image src="/icons/arrow-right.svg" alt="arrow-right" width={16} height={16} />
                   </p>
+
                   <h1 className="head-text">
                      Unleash the Power of
                      <span className="text-primary"> Pricewise</span>
                   </h1>
-                  <p className="mt-6 leading-tight">
+
+                  <p className="mt-6">
                      Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
                   </p>
 
@@ -32,8 +39,8 @@ const Home = () => {
             <h2 className="section-text">Trending</h2>
 
             <div className="flex flex-wrap gap-x-8 gap-y-16">
-               {["Apple Iphone 15", "Book", "Sneakers"].map((product) => (
-                  <div key={product}>{product}</div>
+               {allProducts?.map((product) => (
+                  <ProductCard key={product._id} product={product} />
                ))}
             </div>
          </section>
